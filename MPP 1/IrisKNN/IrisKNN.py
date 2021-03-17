b0f2752834dff60488721686a9613fcb42fad35d
Michal Romanczyk <s20422@pjwstk.edu.pl>
2021-03-17 09:58:14 +0100

    Test

---
+++ MPP 1/IrisKNN/IrisKNN.py
@@ -0,0 +1,13 @@
+print('s20422 Iris K-NN');
+
+#imports
+import numpy as np
+import pandas as pd
+
+#prepare dataset
+dataset = pd.read_csv('iris.csv', sep=';' ,names = ["Sepal_Length", "Sepal_Width", "Petal_Length", "Petal_Width", "Class"])
+
+#print(len(dataset))
+#print(dataset.head())
+
+print(dataset)
