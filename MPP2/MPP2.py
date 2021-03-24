98bb83ebd5e7d9196e8739676b0d19288877ce6f
Michal Romanczyk <s20422@pjwstk.edu.pl>
2021-03-24 22:48:26 +0100

    Update

--- MPP2/MPP2.py
+++ MPP2/MPP2.py
@@ -11,43 +11,67 @@ class Perceptron:
         self.alpha = alpha
     
     def decide(self, X):
-        if (np.dot(self.wagi, X) >= self.theta):
+        if (calc_dot(self.wagi, X) >= self.theta):
             return 1
         return 0
 
     def train(self, X, poprawna):
-        X_vec = X.values.tolist()
-        poprawna_vec = poprawna.values.tolist()
-
-        for i in range(len(X_vec)):
-            y = self.decide(X_vec[i])
-            self.delta(X_vec[i], poprawna[i], y)
+        for i in range(len(X)):
+            y = self.decide(X[i])
+            self.delta(X[i], poprawna[i], y)
 
     def delta(self, X_vec, decyzja_prawidlowa, faktyczna_decyzja):
         #W` = W + (D - Y)aX
         for i in range(len(self.wagi)):
-            self.wagi[i] += float(decyzja_prawidlowa - faktyczna_decyzja) * float(self.alpha) * float(X_vec[i])
+            self.wagi[i] += (decyzja_prawidlowa - faktyczna_decyzja) * self.alpha * X_vec[i]
 
         #theta
-        self.theta += float(decyzja_prawidlowa - faktyczna_decyzja) * float(self.alpha)
+        self.theta += (decyzja_prawidlowa - faktyczna_decyzja) * self.alpha
     
 def start_test(perceptron, testset):
-    testset_vec = testset.values.tolist()
     decisions = []
     for i in range(len(testset)):
         vecX = []
-        decisions.append(perceptron.decide(testset_vec[i]))
+        decisions.append(perceptron.decide(testset[i]))
     return decisions
 
 def calculate_acc(testset_ans, my_ans):
-    testset_ans_vec = testset_ans.values.tolist()
     correct = 0
 
     for i in range(len(my_ans)):
-        if (testset_ans_vec[i] == my_ans[i]):
+        if (testset_ans[i] == my_ans[i]):
             correct += 1
 
-    return (correct/float(len(my_ans))) * 100.0
+    return (correct / float(len(my_ans))) * 100.0
+
+def calc_dot(W, X):
+    sum = 0
+    for i in range(len(W)):
+        sum += W[i] * X[i]
+    return sum
+
+def standarize(X):
+    #formula ze strony
+    #https://machinelearningmastery.com/standardscaler-and-minmaxscaler-transforms-in-python/
+    X_res = X
+
+    count = len(X)
+    for i in range(len(X[0])):
+        sum = 0
+        for j in range(len(X)):
+            sum += X[j][i]
+        mean = sum / count
+
+        sum_sd = 0 
+        for j in range(len(X)):
+            sum_sd += (X[j][i] - mean) ** 2
+        standard_deviation = np.sqrt(sum_sd / count)
+
+        for j in range(len(X)):
+            X_res[j][i] = ((X[j][i] - mean) / standard_deviation)
+
+    return X_res
+
 
 def main():
     if (len(sys.argv[1:]) != 3):
@@ -55,23 +79,32 @@ def main():
         sys.exit()
 
     dataset = pd.read_csv(sys.argv[1])
-    alpha = sys.argv[2]
-    t = sys.argv[3]
+    alpha = float(sys.argv[2])
+    t = float(sys.argv[3])
     
     #dzielenie na podzbiory
     trainset = dataset.sample(frac = 0.75).reset_index(drop=True)
     testset = dataset.drop(trainset.index).reset_index(drop=True)
     
+    X_train = standarize([sublist[0:-1] for sublist in trainset.values.tolist()])
+    poprawna_train = [sublist[-1] for sublist in trainset.values.tolist()]
+    
+    X_test = standarize([sublist[0:-1] for sublist in testset.values.tolist()])
+    poprawna_test = [sublist[-1] for sublist in testset.values.tolist()]
+    
     perceptron = Perceptron(len(dataset.columns) - 1, alpha)
-    accc = []
-    for i in range(50):
-        perceptron.train(trainset.iloc[:, 0:8], trainset.iloc[:, -1])
-        my_dec = start_test(perceptron, testset.iloc[:, 0:8])
-        acc = calculate_acc(testset.iloc[:, -1], my_dec)
-        print("Test #" + str(i), acc)
-        accc.append(acc)
 
-    plt.plot(range(50), accc) 
+    #petla
+    n = 100
+    accset = []
+    for i in range(n):
+        perceptron.train(X_train, poprawna_train)
+        my_dec = start_test(perceptron, X_test)
+        acc = calculate_acc(poprawna_test, my_dec)
+        print("Test #" + str(i), acc)
+        accset.append(acc)
+
+    plt.plot(range(n), accset) 
     plt.show()
 
     
