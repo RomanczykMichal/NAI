90039e84926467e9ea1f5b17b847bd333091a6ba
Michal Romanczyk <s20422@pjwstk.edu.pl>
2021-03-29 19:41:50 +0200

    Update 2

--- MPP2/MPP2.py
+++ MPP2/MPP2.py
@@ -25,15 +25,15 @@ class Perceptron:
         #W` = W + (D - Y)aX
         for i in range(len(self.wagi)):
             self.wagi[i] += (decyzja_prawidlowa - faktyczna_decyzja) * self.alpha * X_vec[i]
-
         #theta
         self.theta += (decyzja_prawidlowa - faktyczna_decyzja) * self.alpha
+        
+        self.wagi = normalize_vec(self.wagi)
     
 def start_test(perceptron, testset):
     #metoda, ktora jest odpowiedzialna za przeprowadzenie testu
     decisions = []
     for i in range(len(testset)):
-        vecX = []
         decisions.append(perceptron.decide(testset[i]))
     return decisions
 
@@ -52,13 +52,20 @@ def calc_dot(W, X):
         sum += W[i] * X[i]
     return sum
 
+def normalize_vec(X):
+    X_norm = []
+    dlugosc = np.sqrt(calc_dot(X, X))
+    for i in range(len(X)):
+        X_norm.append(X[i] / dlugosc)
+    return X_norm
+
 def standarize(X):
     #formula ze strony (bez niej wyniki były jeszcze bardziej absurdalne i bardzo rozstrzelone)
     #https://machinelearningmastery.com/standardscaler-and-minmaxscaler-transforms-in-python/
     X_res = X
 
     count = len(X)
-    for i in range(len(X[0])):
+    for i in range(len(X[0]) - 1):
         sum = 0
         for j in range(len(X)):
             sum += X[j][i]
@@ -80,37 +87,69 @@ def main():
         print("Zla liczba argumentow - wymagane 3")
         sys.exit()
 
-    dataset = pd.read_csv(sys.argv[1])
+    data = pd.read_csv(sys.argv[1])
     alpha = float(sys.argv[2])
     t = float(sys.argv[3])
     
     #dzielenie na podzbiory i standaryzacja
+    datastand = standarize(data.values.tolist())
+    dataset = pd.DataFrame(datastand)
+
     trainset = dataset.sample(frac = 0.75).reset_index(drop=True)
     testset = dataset.drop(trainset.index).reset_index(drop=True)
     
-    X_train = standarize([sublist[0:-1] for sublist in trainset.values.tolist()])
+    X_train = [sublist[0:-1] for sublist in trainset.values.tolist()]
     poprawna_train = [sublist[-1] for sublist in trainset.values.tolist()]
     
-    X_test = standarize([sublist[0:-1] for sublist in testset.values.tolist()])
+    X_test = [sublist[0:-1] for sublist in testset.values.tolist()]
     poprawna_test = [sublist[-1] for sublist in testset.values.tolist()]
     
     perceptron = Perceptron(len(dataset.columns) - 1, alpha)
 
-    #petla
-    n = 300
-    accset = [] #lista zapisanych skutecznosci z kazdego pomiaru
-    for i in range(n):
+    #uczenie
+    n = 1
+    accset = [0]
+
+    while(accset[-1] < t):
         perceptron.train(X_train, poprawna_train)
         my_dec = start_test(perceptron, X_test)
         acc = calculate_acc(poprawna_test, my_dec)
-        print("Test #" + str(i), acc)
         accset.append(acc)
+        print("Test #" + str(n), "Dokladnosc " + str(acc), "\nWagi", perceptron.wagi)
+        n += 1
 
+    print("Perceptron wyuczony na odpowiedni próg.\nWylacz graf zeby kontynuowac")
     plt.plot(range(n), accset)
     plt.xlabel("liczba szkoleń")
     plt.ylabel("skuteczność")
     plt.show()
 
+    #glowna petla
+    while (True):
+        print("Wybierz opcje\n1. Podaj wektor do klasyfikacji.\n2. Wykonaj dodatkowy krok uczenia na podstawie podanego wektora.\n3. Pokaz wage i prog perceptronu.")
+
+        opt = input()
+
+        if (opt == '1'):
+            vec = []
+            for i in range(len(X_train[0])):
+                print("Podaj " + str(i) + " wspolrzedna:", end=" ")
+                vec.append(float(input()))
+            decision = perceptron.decide(vec)
+            print("Decyzja: " + str(decision))
+        elif(opt == '2'):
+            vec = []
+            for i in range(len(X_train[0])):
+                print("Podaj " + str(i) + " wspolrzedna:", end=" ")
+                vec.append(float(input()))
+            print("Podaj poprawny wynik dla tego przypadku.", end=" ")
+            poprawna = float(input())
+            perceptron.train([vec], [poprawna])
+            print("Uczenie udane.")
+        elif(opt == '3'):
+            print("Wagi", perceptron.wagi, "Prog", perceptron.theta)
+        else:
+            break
     
 if __name__ == "__main__":
     main()
